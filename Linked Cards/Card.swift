82cc81f9ca3d68e9ef02cbb77858a2d655677c95
Ben <benduke.nz@gmail.com>
2017-06-16 21:26:24 +1200

    first commit

---
+++ Linked Cards/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Linked Cards
+//
+//  Created by Ben Duke on 10/06/17.
+//  Copyright © 2017 Ben Duke. All rights reserved.
+//
+
+import Foundation
---
+++ Linked Cards/CardCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CardCellTableViewCell.swift
+//  Linked Cards
+//
+//  Created by Ben Duke on 10/06/17.
+//  Copyright © 2017 Ben Duke. All rights reserved.
+//
+
+import UIKit
+
+class CardCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
